3a4feca285cefcb949cd58521971ffcfe1e01503
Geraldo157 <gsmecanica76@gmail.com>
2023-04-09 19:00:36 -0300

    Adicionando novas paginas

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, LoginView, NewPasswordView, ResetPasswordView
 
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name="register")
+    path('register/', RegistrationView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('reset-password/', ResetPasswordView.as_view(), name="reset-password"),
+    path('newpassword/', NewPasswordView.as_view(), name="newpassword"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,3 +5,18 @@ from django.views import View
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
+
+
+class ResetPasswordView(View):
+    def get(self, request):
+        return render(request, 'authentication/reset-password.html')
+
+
+class NewPasswordView(View):
+    def get(self, request):
+        return render(request, 'authentication/set-newpassword.html')
