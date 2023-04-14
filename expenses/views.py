9e1458674ce1cd1cd87a2aff6b91f4f3a1ef88f9
Geraldo157 <gsmecanica76@gmail.com>
2023-04-14 14:51:53 -0300

    Creation of the add-expenses page

--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 
 @login_required(login_url='auth/login/')
 def index(request):
-    return render(request, 'expenses/index.html')
+    exps = Expense.objects.all()
+    context = {
+        'expenses': exps
+    }
+    return render(request, 'expenses/index.html', context)
 
 
 def add_expenses(request):
-    return render(request, 'expenses/add_expenses.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+        else:
+            messages.success(request, 'New expense added')
+        return render(request, 'expenses/add_expenses.html', context)
+    return render(request, 'expenses/add_expenses.html', context)
+
+
+
