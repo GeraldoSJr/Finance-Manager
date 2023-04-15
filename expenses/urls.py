a6ed546a91c6fa913307a684ecb998412fa35b76
Geraldo157 <gsmecanica76@gmail.com>
2023-04-14 22:24:37 -0300

    Creating the update page and implementing delete functionality

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add-expenses/',views.add_expenses, name="add-expenses")
+    path('add-expenses/', views.add_expenses, name="add-expenses"),
+    path('expense-edit/<int:id>/', views.expense_edit, name="expense-edit"),
+    path('delete_expenses/<int:id>/', views.delete_expenses, name="delete_expenses")
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -14,6 +14,42 @@ def index(request):
     return render(request, 'expenses/index.html', context)
 
 
+def expense_edit(request, id):
+    categories = Category.objects.all()
+    expense = Expense.objects.get(pk=id)
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories
+    }
+    if request.method == 'GET':
+        messages.info(request, 'Handling post form')
+        return render(request, 'expenses/edit_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        description = request.POST['description']
+        if request.POST['expense_date'] != "":
+            date = request.POST['expense_date']
+        else:
+            date = now()
+        category = request.POST['category']
+        if not amount:
+            messages.error(request, 'Amount is required')
+
+        expense.owner = request.user
+        expense.amount = amount
+        expense.date = date
+        expense.description = description
+        expense.category = category
+
+        expense.save()
+        messages.success(request, 'Expense Updated')
+
+        return redirect('expenses')
+
+
 def add_expenses(request):
     categories = Category.objects.all()
     context = {
@@ -41,4 +77,8 @@ def add_expenses(request):
         return redirect('expenses')
 
 
-
+def delete_expenses(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, 'Expense deleted')
+    return redirect('expenses')
