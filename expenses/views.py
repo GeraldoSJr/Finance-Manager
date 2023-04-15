591b872d2f8f6f2eb6c8d4191e9aab354649a392
Geraldo157 <gsmecanica76@gmail.com>
2023-04-15 10:25:44 -0300

    Created the expenses paginator

--- expenses/views.py
+++ expenses/views.py
@@ -3,13 +3,18 @@ from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
 from django.utils.timezone import now
+from django.core.paginator import Paginator
 
 
 @login_required(login_url='auth/login/')
 def index(request):
     expense = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expense, 4)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
     context = {
-        'expense': expense
+        'expense': expense,
+        'page_obj': page_obj
     }
     return render(request, 'expenses/index.html', context)
 
