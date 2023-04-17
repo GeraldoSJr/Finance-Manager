482406fbc762be509f9b588de35b4306b1702d19
Geraldo157 <gsmecanica76@gmail.com>
2023-04-17 18:16:45 -0300

    Creating the search engine of the expenses page

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name="expenses"),
     path('add-expenses/', views.add_expenses, name="add-expenses"),
     path('expense-edit/<int:id>/', views.expense_edit, name="expense-edit"),
-    path('delete_expenses/<int:id>/', views.delete_expenses, name="delete_expenses")
+    path('delete_expenses/<int:id>/', views.delete_expenses, name="delete_expenses"),
+    path('search-expenses/', csrf_exempt(views.search_expenses), name="search-expenses"),
 ]
